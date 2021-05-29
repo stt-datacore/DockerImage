720ae2ce1f5d341847b595bd3c317e4a4566489c
Effy Elden <git@effy.is>
2021-05-29 22:39:52 +1000

    Tweak config

--- runme.sh
+++ runme.sh
@@ -19,8 +19,19 @@
 echo "Configuring PM2"
 source /data/config/env
 pm2 install pm2-discord-webhook
-pm2 set pm2-discord-webhook:discord_url_logs ${PM2_INFO_URL}
-pm2 set pm2-discord-webhook:discord_url_errors ${PM2_ERROR_URL}
+
+# Don't forward stdout - too spammy
+pm2 set pm2-discord-webhook:log false
+# Do forward stderr - useful
+pm2 set pm2-discord-webhook:error true
+# Send messages when service starts or restarts
+pm2 set pm2-discord-webhook:start true
+pm2 set pm2-discord-webhook:online true
+pm2 set pm2-discord-webhook:stop true
+pm2 set pm2-discord-webhook:restart true
+# Set destinations for webhooks (URLs sourced from env)
+pm2 set pm2-discord-webhook:webhook_url_logs ${PM2_INFO_URL}
+pm2 set pm2-discord-webhook:webhook_url_errors ${PM2_ERROR_URL}
 
 ##################################################
 # Setting up static website content              #
