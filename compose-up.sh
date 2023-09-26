5222c90a72cc8c9b262d8e3aa1e48cff21d1eb2d
Nathaniel Moschkin <ironywrit@gmail.com>
2023-09-26 13:54:43 -0400

    Add compose-down script

--- compose-up.sh
+++ compose-up.sh
@@ -2,4 +2,4 @@
 cd multidocker
 echo Compose Script - Full Datacore Stack
 echo     Note: add -d if you wish to start detached
-docker-compose --profile tower-stack up --build $1
+sudo docker compose --profile tower-stack up --build $1
