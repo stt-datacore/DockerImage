7ebe92e4430601a3ceb2fb84f84a972ad66fa982
Nathaniel Moschkin <precise.tester@gmail.com>
2023-10-03 17:19:34 -0400

    Mongo docker volume

--- compose-check.sh
+++ compose-check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd multidocker
-sudo docker compose --profile tower-stack exec tower-stack docker-compose -f /docker/docker-compose.yml --profile fullstack up --build
+sudo docker compose --profile tower-stack exec tower-stack docker-compose -f /docker/docker-compose.yml --profile fullstack up --build $1
 
